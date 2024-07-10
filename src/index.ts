0d3d9e2026541558e916012f2a0a57184e7cb437
YamenMerhi <yamennmerhi@gmail.com>
2024-07-11 00:54:37 +0300

    feat: include recoverRawTransaction in package

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 // Types
 export { DeploymentConfig, DeploymentResult } from './types/deploymentConfig';
 export { TransactionConfig, TransactionResult } from './types/transactionConfig';
+export { RecoveredTransactionResult } from './types/rawTransactionConfig';
 
 // Functions
 export { genRawDeployment } from './genRawDeployment/genRawDeployment';
 export { genRawTransaction } from './genRawTransaction/genRawTransaction';
+export { recoverRawTransaction } from './recoverRawTransaction/recoverRawTransaction';
